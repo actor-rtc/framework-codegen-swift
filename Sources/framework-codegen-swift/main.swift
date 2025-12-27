3f5c7e75fa3fa2a66e1950521095ae260b96fbc0
Harry <128583280+kaito-harry@users.noreply.github.com>
2025-12-27 16:10:59 +0800

    chore: bump version to 0.1.5

--- Sources/framework-codegen-swift/main.swift
+++ Sources/framework-codegen-swift/main.swift
@@ -3,7 +3,7 @@ import SwiftProtobufPluginLibrary
 
 @main
 struct ActrFrameworkGenerator {
-    static let version = "0.1.4"
+    static let version = "0.1.5"
 
     static func main() throws {
         // Handle command line arguments
